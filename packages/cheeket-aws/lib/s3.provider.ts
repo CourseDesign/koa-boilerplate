1765c5446d06a1778cfcb5278818b16efcdd7f32
Ara Park <kdPark0723@gmail.com>
2021-03-01 11:14:33 +0900

    feat: aws (#4)
    * init
    
    * add providers
    
    * modules-> packages
    
    * fix error

---
+++ packages/cheeket-aws/lib/s3.provider.ts
@@ -0,0 +1,10 @@
+import { interfaces } from "cheeket";
+import * as AWS from "aws-sdk";
+
+function s3Provider(
+  configuration: AWS.S3.ClientConfiguration
+): interfaces.Provider<AWS.S3> {
+  return () => new AWS.S3(configuration);
+}
+
+export default s3Provider;
