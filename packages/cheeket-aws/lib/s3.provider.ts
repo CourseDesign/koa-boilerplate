3074592a25a9d27c0920e4c0646fc26ccee0d03b
siyual.park <siyual.bak@gmail.com>
2021-03-02 18:10:36 +0900

    use mock

--- packages/cheeket-aws/lib/s3.provider.ts
+++ packages/cheeket-aws/lib/s3.provider.ts
@@ -1,10 +1,16 @@
 import { interfaces } from "cheeket";
 import * as AWS from "aws-sdk";
+import S3Mock from "./s3-mock";
 
 function s3Provider(
-  configuration: AWS.S3.ClientConfiguration
+  configuration: AWS.S3.ClientConfiguration | { mock: true }
 ): interfaces.Provider<AWS.S3> {
-  return () => new AWS.S3(configuration);
+  return () => {
+    if ((configuration as { mock: true }).mock) {
+      return new S3Mock();
+    }
+    return new AWS.S3(configuration as AWS.S3.ClientConfiguration);
+  };
 }
 
 export default s3Provider;
