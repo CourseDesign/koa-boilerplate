003b91e843ee2e9e108d0bc7a9f36368933c6dd4
siyual.park <siyual.bak@gmail.com>
2021-03-02 17:38:46 +0900

    Add serializable

--- commons/util-types/lib/index.ts
+++ commons/util-types/lib/index.ts
@@ -1,2 +1,6 @@
 export { default as Type } from "./type";
+
+export { default as Serializable } from "./serializable";
+export { default as Deserializable } from "./deserializable";
+
 export { default as staticImplements } from "./static-implements.decorator";
