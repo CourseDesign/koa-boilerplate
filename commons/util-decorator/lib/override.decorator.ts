fe56eb78e156d93490f8842d73e14f24d6adb950
siyual.park <siyual.bak@gmail.com>
2021-03-03 14:32:22 +0900

    fix error

--- commons/util-decorator/lib/override.decorator.ts
+++ commons/util-decorator/lib/override.decorator.ts
@@ -1,14 +1,3 @@
-const override: MethodDecorator = (target, propertyKey) => {
-  if (process.env.NODE_ENV !== "production") {
-    const baseType = Object.getPrototypeOf(target);
-    if (typeof baseType[propertyKey] !== "function") {
-      throw new Error(
-        `Method ${propertyKey.toString()} of ${
-          target.constructor.name
-        } does not override any base class method`
-      );
-    }
-  }
-};
+const override: MethodDecorator = () => {};
 
 export default override;
