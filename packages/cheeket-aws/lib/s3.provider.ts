4e00a076b0e60eb11f474ef9f511991fea174704
siyual.park <siyual.bak@gmail.com>
2021-03-13 15:23:22 +0900

    remove s3 mock

--- packages/cheeket-aws/lib/s3.provider.ts
+++ packages/cheeket-aws/lib/s3.provider.ts
@@ -1,15 +1,12 @@
 import { interfaces } from "cheeket";
 import * as AWS from "aws-sdk";
-import S3Mock from "./s3-mock";
 
 function s3Provider(
-  configuration: AWS.S3.ClientConfiguration | { mock: true }
+  configuration: AWS.S3.ClientConfiguration
 ): interfaces.Provider<AWS.S3> {
   return () => {
-    if ((configuration as { mock: true }).mock) {
-      return new S3Mock();
-    }
-    return new AWS.S3(configuration as AWS.S3.ClientConfiguration);
+
+    return new AWS.S3(configuration);
   };
 }
 
