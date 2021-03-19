66695a28d3c06e351ea36c33e73dd63b47d8e191
siyual.park <siyual.bak@gmail.com>
2021-03-19 15:49:50 +0900

    remove http-errors

--- commons/http-errors/lib/bad-reqeust-error.ts
+++
@@ -1,9 +0,0 @@
-import HttpError from "./http-error";
-
-class BadRequestError extends HttpError {
-  constructor(message?: string, public readonly details?: string[]) {
-    super(400, message);
-  }
-}
-
-export default BadRequestError;
