91a3bda5968a3ffcd6b34d90d843efab3df69d50
Ara Park <siyual.bak@gmail.com>
2021-03-20 14:53:28 +0900

    feat: remove utils (#33)
    * remove utils
    
    * update lock

--- applications/network/lib/dependency.initializer.ts
+++ applications/network/lib/dependency.initializer.ts
@@ -1,6 +1,6 @@
 import { Initializer } from "@cheeket/koa";
 import { interfaces } from "cheeket";
-import { override } from "@course-design/util-decorator";
+import { override } from "@course-design/decorators";
 import { LoggerDependencyInitializer } from "./logger";
 
 class DependencyInitializer implements Initializer {
