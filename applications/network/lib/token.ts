2a31863520323524f2c319d2df1c5c5c6dfac8f3
siyual-park <kdpark0723@gmail.com>
2021-03-27 12:28:34 +0900

    change token name

--- applications/network/lib/token.ts
+++ applications/network/lib/token.ts
@@ -2,10 +2,10 @@ import { interfaces } from "cheeket";
 import { Logger, transport as Transport } from "winston";
 import { LoggerToken } from "./middleware";
 
-const Token: LoggerToken = Object.freeze({
+const token: LoggerToken = Object.freeze({
   rootLogger: Symbol("Logger@RootLogger") as interfaces.Token<Logger>,
   logger: Symbol("Logger@Logger") as interfaces.Token<Logger>,
   transport: Symbol("Logger@Transport") as interfaces.Token<Transport>,
 });
 
-export default Token;
+export default token;
