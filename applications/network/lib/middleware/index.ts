6a2d7bd6602161b45e604f01e06a0fc8d7159d23
siyual-park <kdpark0723@gmail.com>
2021-03-28 09:18:11 +0900

    feat: dependency => logger

--- applications/network/lib/middleware/index.ts
+++ applications/network/lib/middleware/index.ts
@@ -4,4 +4,4 @@ export { default as State } from "./state";
 export { default as pingPongMiddleware } from "./ping-pong.middleware";
 export { default as versionMiddleware } from "./version.middleware";
 
-export * from "./dependency";
+export * from "./logger";
