1b98866a88daccdf9a30d9748f7c9f347085c826
siyual-park <kdpark0723@gmail.com>
2021-05-21 19:07:26 +0900

    fix

---
+++ applications/network/lib/configuration-provider.ts
@@ -0,0 +1,19 @@
+import {ApplicationConfiguration} from "./bootstrap";
+import envConfig from "./env-config";
+
+class ConfigurationProvider {
+  private readonly config: ApplicationConfiguration
+
+  constructor(config: Partial<ApplicationConfiguration>) {
+    this.config = {
+      port: config.port ?? envConfig.port,
+      container: config.container ?? envConfig.container
+    }
+  }
+
+  get(): ApplicationConfiguration {
+    return this.config
+  }
+}
+
+export default ConfigurationProvider;
