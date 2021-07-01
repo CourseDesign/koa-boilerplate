329f18583389b38b599d3db245e65a39d8d39672
siyual-park <kdpark0723@gmail.com>
2021-07-01 20:15:42 +0900

    add filter

--- applications/server/lib/bootstrap.ts
+++ applications/server/lib/bootstrap.ts
@@ -9,18 +9,28 @@ import { query, request, response } from "koa-position";
 import requestId from "koa-requestid";
 import serialize from "koa-serialize";
 import expose from "koa-expose";
+import { filter } from "koa-logic";
+import compose from "koa-compose";
 import { dependency } from "@cheeket/koa";
 
 import routes from "./routes";
 import { logger } from "./module";
 import { Config } from "./config";
+import { isRequestType, isResponseType } from "./expression";
+
+const requestIdHeader = "Request-ID";
+
+const isRequestTypeJson = isRequestType("application/json");
+const isResponseTypeJson = isResponseType("application/json");
 
 async function bootstrap(config: Config): Promise<Server> {
   const application = new Application();
 
   koaQs(application);
 
-  application.use(requestId());
+  application.use(
+    requestId({ expose: requestIdHeader, header: requestIdHeader })
+  );
 
   application.use(dependency(config.container));
 
@@ -28,15 +38,22 @@ async function bootstrap(config: Config): Promise<Server> {
 
   application.use(bodyParser());
   application.use(camelCase(query()));
-  application.use(camelCase(request("body")));
+  application.use(filter(isRequestTypeJson, camelCase(request("body"))));
 
   const router = routes();
   application.use(router.routes());
   application.use(router.allowedMethods());
 
-  application.use(serialize(response("body")));
-  application.use(snakeCase(response("body")));
-  application.use(expose(query("fields")));
+  application.use(
+    filter(
+      isResponseTypeJson,
+      compose([
+        serialize(response("body")),
+        snakeCase(response("body")),
+        expose(query("fields")),
+      ])
+    )
+  );
 
   return application.listen(config.port);
 }
