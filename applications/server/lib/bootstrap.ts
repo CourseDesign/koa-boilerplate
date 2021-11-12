e36290954bc614a8fc0786fabe1f247cc17f8c3e
ara.bak <siyual.bak@gmail.com>
2021-11-12 21:12:18 +0900

    Add router

--- applications/server/lib/bootstrap.ts
+++ applications/server/lib/bootstrap.ts
@@ -1,12 +1,19 @@
 /* eslint-disable no-param-reassign */
 
-import Application from "koa";
 import { Server } from "net";
+import Application from "koa";
 import { Container } from "cheeket";
 import { dependency } from "cheeket-koa";
+import { snakeCase, camelCase } from "koa-change-case";
+import bodyParser from "koa-bodyparser";
+import { request, response } from "koa-position";
+import { filter, finalize } from "koa-logic";
 
 import Config, { ConfigProvider } from "./config";
 import rootRouter from "./router";
+import { isRequestType, isResponseType } from "./expression";
+
+const jsonType = "application/json";
 
 async function bootstrap(config?: Config): Promise<Server> {
   if (config === undefined) {
@@ -19,6 +26,12 @@ async function bootstrap(config?: Config): Promise<Server> {
   const router = rootRouter();
 
   application.use(dependency(container));
+  application.use(
+    finalize(filter(isResponseType(jsonType), snakeCase(response("body"))))
+  );
+
+  application.use(bodyParser());
+  application.use(filter(isRequestType(jsonType), camelCase(request("body"))));
 
   application.use(router.routes());
   application.use(router.allowedMethods());
