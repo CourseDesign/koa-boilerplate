027ddf7608e355c86a8eff05b58df949f8a229c2
ara.bak <siyual.bak@gmail.com>
2021-11-12 19:59:14 +0900

    refactoring

--- applications/server/lib/bootstrap.ts
+++ applications/server/lib/bootstrap.ts
@@ -1,9 +1,15 @@
+/* eslint-disable no-param-reassign */
+
 import { Server } from "net";
 import Application from "koa";
 
-import Config from "./config";
+import Config, { ConfigProvider } from "./config";
 
-async function bootstrap(config: Config): Promise<Server> {
+async function bootstrap(config?: Config): Promise<Server> {
+  if (config === undefined) {
+    config = new ConfigProvider().get();
+  }
+
   const application = new Application();
   return application.listen(config.port);
 }
