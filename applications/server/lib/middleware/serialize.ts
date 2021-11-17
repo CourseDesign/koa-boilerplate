7b862a0baa5fee2e70e64c43a8a8aa9bed7d7ca0
Ara Park <siyual.bak@gmail.com>
2021-11-17 18:20:27 +0900

    refactoring

--- applications/server/lib/middleware/serialize.ts
+++ applications/server/lib/middleware/serialize.ts
@@ -3,6 +3,7 @@ import { filter, finalize } from "koa-logic";
 import { camelCase, snakeCase } from "koa-change-case";
 import { query, request, response } from "koa-position";
 import bodyParser from "koa-bodyparser";
+import toJson from "koa-serialize";
 
 import { isRequestType, isResponseType } from "../expression";
 import { Middleware } from "../type";
@@ -11,7 +12,15 @@ const json = "application/json";
 
 function serialize(): Middleware {
   return compose([
-    finalize(filter(isResponseType(json), snakeCase(response("body")))),
+    finalize(
+      filter(
+        isResponseType(json),
+        compose([
+          toJson(response("body"), (value) => value),
+          snakeCase(response("body")),
+        ])
+      )
+    ),
     bodyParser(),
     camelCase(query()),
     filter(isRequestType(json), camelCase(request("body"))),
